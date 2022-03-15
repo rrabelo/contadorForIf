77cbf1fb6979484e20925b9ffe10b5f0556ce153
Rafael Rabelo <60629367+rrabelo@users.noreply.github.com>
2022-03-14 23:02:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!!')
+        res.innerHTML = `impossível contar`
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if( p <= 0) {
+            window.alert('Passo Inválido, considera 1.')
+            p=1
+        }
+
+        if (i < f){
+            //contagem crescente
+            for( let c = 1; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else { 
+            //contagem regressiva
+            for( let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }    
+         res.innerHTML +=`\u{1F3C1}`
+    }        
+}
